4ed622a8c7da0ea4df1fee6f882e71d11bb0c4af
Jaswanth Reddy Rachapalli <114978764+JaswanthReddy325@users.noreply.github.com>
2025-01-26 20:54:13 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithPopup, sendPasswordResetEmail, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth, provider } from "../firebaseConfig"; // Firebase configuration
